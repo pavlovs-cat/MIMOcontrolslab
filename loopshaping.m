ff9c56b1f9333e617114764bf287825149865df2
Yigit Yakupoglu <yyakupog@andrew.cmu.edu>
2018-04-24 15:15:34 -0400

    Fix Issues with W, delta
    Talked to Mark. Figured out different weights for each and Delta. Plotted slower responses to square waves, and new 2,2 Gnom. Inv Loop Shape q2 works now.

--- loopshaping.m
+++ loopshaping.m
@@ -1,21 +1,21 @@
 preliminaries % load uncertain plant
 %% Loop Shaping
 s = tf('s');
-wc = 5;
+wc = 1;
 Ld = eye(2)*(wc/s); %desired loopshape
 A_nom=A.NominalValue
 B_nom=B.NominalValue
-K=minreal(inv(G_nom)*10000000/((s+1000)^2)*(Ld));
-L=minreal(G_nom*K);
-CL=(L)/(eye(2)+L)
+K=10*(1/(s/1000+1))^2*inv(G_nom)*(Ld);
+L=G_nom*K;
+%CL=(L)/(eye(2)+L)
+%L=K*G_nom
+%[K,CL,GAM,INFO]=loopsyn(G_nom,Ld)
 
-W_I_Pitch=Wt;
-W_I_Yaw=Wt;
-sampled_plant=usample(G_unc,1)
-Dsamp=(sampled_plant-G_nom)/Wt
-Delta_1=Dsamp(1,1);
-Delta_2=Dsamp(2,2);
-%Delta_1=rand(0,1);
-%Delta_2=rand(0,1);
+
 %K=[(1711*s+4910)/(s+50)  (-1557*s-5153)/(s+50)
 %    (2432*s+7817)/(s+50)  (921.5*s+3308)/(s+50)];
+D1=ultidyn('D1',[1 1]);
+D2=ultidyn('D2',[1 1]);
+Delta_1=usample(D1);
+Delta_2=usample(D2);
+%ncfsyn gkinverse
