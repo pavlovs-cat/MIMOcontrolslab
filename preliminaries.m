cd0b423f81b56c98ec89a286285545a483444ae0
DESKTOP-8CKVN2C\Catherine <catherinepavlov@gmail.com>
2018-04-24 10:12:37 -0400

    Updated preliminaries

--- preliminaries.m
+++ preliminaries.m
@@ -9,10 +9,11 @@ run('quanser_aero_state_space.m')
 %% Find Poles & Zeros
 s=tf('s')
 G_nom=minreal(C*inv(s*eye(4)-A.NominalValue)*B.NominalValue+D)
-zeros=tzero(G_nom)
-poles=eig(G_nom)
+zeros=tzero(G_nom);
+poles=eig(G_nom);
 %% Uncertain Plant
-G_unc=C*inv(s*eye(4)-A)*B+D;
+G_unc=C*inv(s*eye(4)-A)*B+D
 G_vec=usample(G_unc,50);
 [usys,info]=ucover(G_vec,G_nom,2);
 bodemag(G_vec,G_nom*(eye(2)+info.W1*info.W2));
+Wt = info.W1*info.W2
