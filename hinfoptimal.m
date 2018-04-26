b82c180ca11b5d6bf2efc033bbf669f7042ac48e
pavlovs-cat <catherinepavlov@gmail.com>
2018-04-25 21:46:11 -0400

    Added files for more controllers

---
+++ hinfoptimal.m
@@ -0,0 +1,33 @@
+%preliminaries
+
+Wt = []; %Not sure what sensitivity weight should be
+Wu = 1/25*eye(2); %control weight
+
+w_max = 100;
+w_min = 0;
+w_try = (w_max+w_min)/2;
+tol = .01;
+
+%maximize wc
+while(w_max-w_min > tol)
+    Wp = makeweight(100, w_try, 1/3)*eye(2); %performance weight
+    P = augw(G_nom, Wp, Wu, Wt);
+    [Kinf,CL,GAM] = hinfsyn(P,2,2);
+    
+    if GAM > 1
+        w_max = w_try;
+    else
+        w_min = w_try;
+    end
+    w_try = (w_max + w_min);
+end
+
+%Check RP + RS
+Sinf = eye(2)-feedback(Gunc*Kinf,eye(2));
+bodemag(Sinf,inv(Wp))
+[STABMARG,DESTABUNC,REPORT,INFO] = robuststab(Sinf)
+
+D1=ultidyn('D1',[1 1]);
+D2=ultidyn('D2',[1 1]);
+Delta_1=usample(D1);
+Delta_2=usample(D2);
---
+++ musyn.m
@@ -0,0 +1,4 @@
+preliminaries
+
+Wu = 1/25*eye(2);
+Wp = makeweight(100, wc, 1/3)*eye(2);
